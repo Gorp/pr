6bdfc6383e64744a1e47f278dcbd2a59280b2b61
Dima home <digorp@gmail.com>
2010-11-15 23:15:24 +0200

    головний контролер меню

--- library/Local/Controller.php
+++ library/Local/Controller.php
@@ -7,7 +7,10 @@ class Local_Controller extends Zend_Controller_Action {
         $bootstrap = $frontController->getParam('bootstrap');
 
         $this->view->config = new Zend_Config($bootstrap->getOptions());
+        $this->view->headerurl = '/public/img/header_main.jpg';
 
+        // отримуємо головне меню
+        $this->view->menu = Model_Menu::getAll(0);
         
     }
 }
